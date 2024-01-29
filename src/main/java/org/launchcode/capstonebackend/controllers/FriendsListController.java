baab698d63f1d86f4d83ecb6c9363a040dca6f68
Angela Engle <angelaengle67@gmail.com>
2024-01-29 17:10:15 -0600

    Changed friends list controller to RESTful

--- src/main/java/org/launchcode/capstonebackend/controllers/FriendsListController.java
+++ src/main/java/org/launchcode/capstonebackend/controllers/FriendsListController.java
@@ -2,14 +2,16 @@ package org.launchcode.capstonebackend.controllers;
 
 import jakarta.validation.Valid;
 import org.launchcode.capstonebackend.models.FriendsList;
+import org.launchcode.capstonebackend.models.WatchList;
 import org.launchcode.capstonebackend.models.data.FriendsListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("friends")
@@ -17,16 +19,28 @@ public class FriendsListController {
     @Autowired
     private FriendsListRepository friendsListRepository;
 
-    @PostMapping("add")
-    public String processAddFriendForm(@ModelAttribute @Valid FriendsList newFriend,
-                                       Errors errors, Model model) {
+    @GetMapping("get-friendslist")
+    public ResponseEntity<List<FriendsList>> getAllFriends() {
 
-        if (errors.hasErrors()) {
-            return "friends/add";
+        List<FriendsList> friendsList = (List<FriendsList>) friendsListRepository.findAll();
+
+        if (friendsList.isEmpty()) {
+            return ResponseEntity.notFound().build();
         } else {
-            friendsListRepository.save(newFriend);
+            return ResponseEntity.ok(friendsList);
         }
-
-        return "redirect:";
+    }
+    @PostMapping("add-friend")
+    public ResponseEntity<FriendsList> processAddNewFriendForm(@RequestBody @Valid FriendsList newFriendsList, Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest().build();
+        }
+        try {
+            FriendsList savedFriendsList = friendsListRepository.save(newFriendsList);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedFriendsList);
+        } catch (Exception exception) {
+            System.out.println("Error saving FriendsList to database: " + exception.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
